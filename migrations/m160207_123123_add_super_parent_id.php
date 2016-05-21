89f05bf43bbe87aa56e352777678975297faa60c
YeeSoft <makitrataras@gmail.com>
2016-05-21 11:49:36 +0300

    migration fix

--- migrations/m160207_123123_add_super_parent_id.php
+++ migrations/m160207_123123_add_super_parent_id.php
@@ -8,7 +8,7 @@ class m160207_123123_add_super_parent_id extends Migration
     
     public function up()
     {
-        $this->addColumn(self::TABLE_NAME, 'super_parent_id', $this->integer()->comment('null-has no parent, int-1st level parent id')->after('email'));
+        $this->addColumn(self::TABLE_NAME, 'super_parent_id', $this->integer()->comment('null-has no parent, int-1st level parent id'));
         $this->createIndex('comment_super_parent_id', self::TABLE_NAME, 'super_parent_id');
     }
 
